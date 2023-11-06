2391ec5772e98a4b7b656b344dd779a8f2634d3e
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2023-11-06 23:52:07 +0200

    composer upd

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1c72454bda83d916dca3953b5169fc61bd1b21cb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1c72454bda83d916dca3953b5169fc61bd1b21cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
