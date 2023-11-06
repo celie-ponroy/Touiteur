53e85d52a486f7bce3b3b3768b8591b4c4787cf3
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-11-06 15:41:09 +0100

    add user authenticate

--- src/classes/user/UserAuthentifie.php
+++ src/classes/user/UserAuthentifie.php
@@ -1,6 +1,23 @@
 <?php
 
+    /**
+     * déclarations des namespaces
+     */
+    namespace iutnc\touiteur\user;
 
+    class UserAuthentifie extends User{
 
+        /**
+         * déclarations des attributs
+         */
+        protected string $login, $password, $date;
 
-?>
+        /**
+         * Constructeur
+         */
+        public function __construct(string $login, string $password){
+            $this->login = $login;
+            $this->password = $password;
+        }
+
+    }
