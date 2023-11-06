2e3d9a4248591d33a280b00c8d3ca0d9fe9e83d6
Maëlle <maelle.bitsindou2@etu.univ-lorraine.fr>
2023-11-06 16:26:49 +0100

    Formulaire

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
+        'reference' => '2c227324206758394a103ac23fdbf80038cc8d0d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '3567547930b5b565b3143f46f0f2ef04db8b96fd',
-        'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
+            'reference' => '2c227324206758394a103ac23fdbf80038cc8d0d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '3567547930b5b565b3143f46f0f2ef04db8b96fd',
             'dev_requirement' => false,
         ),
     ),
