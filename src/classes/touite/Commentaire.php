3c7591ebbe22b5f21c4f00e77104feb43c09eb3d
Célie Ponroy <celie.ponroy@laposte.net>
2023-11-06 15:20:20 +0100

    ajout de la date

--- src/classes/touite/Commentaire.php
+++ src/classes/touite/Commentaire.php
@@ -1,11 +1,14 @@
 <?php
+declare(strict_types= 1);
+
 namespace iutnc\touiter\touite;
 class Commentaire extends Touite{
     private $touite;
+    
 
     public function __construct(String $texte ,Touite $touite, User $user){
         parent::__construct($texte, $user);
-        $this->touite = $touite;
+        $this->touite = $touite;   
     }
 
 }
