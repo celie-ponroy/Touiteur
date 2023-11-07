be25f270ff441cb0b9384c53e42e3cc5c2aead9d
Célie Ponroy <celie.ponroy@laposte.net>
2023-11-07 10:14:57 +0100

    modif incription pour la bd

--- src/classes/action/InscriptionAction.php
+++ src/classes/action/InscriptionAction.php
@@ -7,9 +7,13 @@ use iutnc\touiteur\bd\ConnectionFactory;
 
 class InscriptionAction extends Action {
 
-
+    protected ?string $http_method = null;
+    protected ?string $hostname = null;
+    protected ?string $script_name = null;
     public function __construct(){
-        parent::__construct();
+        $this->http_method = $_SERVER['REQUEST_METHOD'];
+        $this->hostname = $_SERVER['HTTP_HOST'];
+        $this->script_name = $_SERVER['SCRIPT_NAME'];
     }
     public function execute() : string{
         $html = "";
@@ -17,6 +21,7 @@ class InscriptionAction extends Action {
         if($methode ==='GET'){
             $html =" <form id='f1' action='?action=inscription' method='post'>
             <input type='text' placeholder='<nom>' name='nom'>
+            <input type='text' placeholder='<prenom>' name='prenom'>
             <input type='email' placeholder='<email>' name='email'>
             <input type='text' placeholder='<pass>' name='pass'>
             <button type='submit'>Valider</button>
@@ -24,27 +29,26 @@ class InscriptionAction extends Action {
         }else if ($methode === 'POST') {
             var_dump($_POST);
             echo "<br>";
-            $nom = filter_var($_POST['nom'],FILTER_SANITIZE_STRING);
+            $nom = filter_var($_POST['nom']);
+            $prenom = filter_var($_POST['prenom']);
             $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
             $mdp = password_hash($_POST['pass'], PASSWORD_DEFAULT, ['cost'=> 12]);
-            $date = (new \DateTime())->format('Y-m-d');
-            var_dump($date);
             $role = 1;
 
             $pdo = ConnectionFactory::makeConnection();
 
-            $query = "INSERT INTO USERS (name, email, password, dateRegister, role) VALUES (:nom, :email, :mdp, :date, :role)";
+            $query = "INSERT INTO Utilisateur (nom , prenom, email, password, role) VALUES (:nom,:prenom, :email, :mdp , :role)";
 
             $stmt = $pdo->prepare($query);
 
             $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':prenom', $prenom);
             $stmt->bindParam(':email', $email);
             $stmt->bindParam(':mdp', $mdp);
-            $stmt->bindParam(':date', $date);
             $stmt->bindParam(':role', $role);
 
             if ($stmt->execute()) {
-                $html = " ajout user<br> Email:".$email.", Nom:".$nom.", Date: ".$date;
+                $html = " ajout user<br> Email:".$email.", Nom:".$nom." Prenom:".$prenom;
             } else {
                 $html = "INSERT ERROR: " . $stmt->errorInfo()[2];
             }
