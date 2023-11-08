a99a432166a4871cb6d7f534d78e7926fabbede2
Célie Ponroy <celie.ponroy@laposte.net>
2023-11-08 11:25:55 +0100

    follow

--- src/classes/user/UserAuthentifie.php
+++ src/classes/user/UserAuthentifie.php
@@ -5,6 +5,8 @@
  */
 namespace iutnc\touiteur\user;
 use iutnc\touiteur\bd\ConnectionFactory;
+use iutnc\touiteur\excetion\AbonnementException;
+use iutnc\touiteur\touite\Touite;
 
 class UserAuthentifie extends User{
 
@@ -112,46 +114,84 @@ class UserAuthentifie extends User{
      * Méthode qui permet de suivre un utilisateur entré en paramètre
      */
     public function followUser(User $userToFollow) {
-        // Assurez-vous que $this représente l'utilisateur authentifié.
+        // si l'utilisateur est identifié
         if (self::isUserConnected()) {
             // Ajoutez une entrée dans la table Abonnement pour enregistrer la relation de suivi.
             $db = ConnectionFactory::makeConnection();
 
-            // Vérifiez d'abord si la relation de suivi n'existe pas déjà
+            //on regarde si this suis déjà l'utilisateur
             $sql = "SELECT COUNT(*) FROM Abonnement WHERE idSuivi = :idSuivi AND idAbonné = :idAbonné";
+            //préparation des données 
             $stmt = $db->prepare($sql);
-            $stmt->bindParam(':idSuivi', $userToFollow->__get('id'));
-            $stmt->bindParam(':idAbonné', $this->__get('id'));
+            $idsuiv =  $userToFollow->__get('id');
+            $idabo = $this->__get('id');
+            //attribution des paramètres
+            $stmt->bindParam(':idSuivi', $idsuiv);
+            $stmt->bindParam(':idAbonné', $idabo);
+            //exécution
             $stmt->execute();
 
-            // Si la requête renvoie 0, cela signifie que la relation de suivi n'existe pas encore
+            // Si la requête renvoie 0, cela signifie que la relation de suivi n'existe pas encore, l'utilisateur va follow
             if ($stmt->fetchColumn() == 0) {
                 // La relation de suivi n'existe pas encore, nous pouvons donc l'ajouter
                 $sql = "INSERT INTO Abonnement (idSuivi, idAbonné) VALUES (:idSuivi, :idAbonné)";
-                $stmt = $db->prepare($sql);
-                $stmt->bindParam(':idSuivi', $userToFollow->__get('id'));
-                $stmt->bindParam(':idAbonné', $this->__get('id'));
+            } else {//unfollow
+                // La relation de suivi existe déjà
+                $sql = "DELETE FROM Abonnement WHERE idSuivi = :idSuivi AND idAbonné = :idAbonné";
+            }
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':idSuivi', $idsuiv);
+            $stmt->bindParam(':idAbonné', $idabo); 
 
-                if ($stmt->execute()) {
-                    // Suivi réussi
-                    return true;
-                } else {
-                    // Gérer l'erreur en cas d'échec du suivi
-                    return false;
-                }
+            if ($stmt->execute()) {
+                // Suivi réussi
+                return true;
             } else {
-                // La relation de suivi existe déjà, vous pouvez gérer cette situation comme vous le souhaitez (par exemple, en affichant un message).
-                echo "Vous suivez déjà cet utilisateur.";
-                return false;
+                throw new AbonnementException();
             }
         }
 
     }
 
+    public function followTag(int $idTag) {
+        // si l'utilisateur est identifié
+        if (self::isUserConnected()) {
+            $db = ConnectionFactory::makeConnection();
 
+            //on regarde si this suis déjà le tag
+            $sql = "SELECT COUNT(*) FROM AbonnementTag WHERE idTag = :idTag AND email = :email";
+            //préparation des données 
+            $stmt = $db->prepare($sql);
+            $email = $this->__get('id');
+            //attribution des paramètres
+            $stmt->bindParam(':idTag', $idTag);
+            $stmt->bindParam(':email', $email);
+            //exécution
+            $stmt->execute();
 
+            // Si la requête renvoie 0, cela signifie que la relation de suivi n'existe pas encore, l'utilisateur va follow
+            if ($stmt->fetchColumn() == 0) {
+                $sql = "INSERT INTO AbonnementTag (email, idTag) VALUES (:idTag, :email)";
+            }else{
+                // La relation de suivi existe déjà, l'utilisateur va unfollow:
+                $sql = "DELETE FROM AbonnementTag
+                            WHERE idTag = :idTag AND email = :email ;";
+                
+            }
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':idtag', $idTag);
+            $stmt->bindParam(':email', $email); 
 
-
+            if ($stmt->execute()) {
+                // Suivi réussi
+                return true;
+            } else {
+                //échec du suivi
+                //throw exception
+                throw new AbonnementException();
+            }
+        }
+    }
 
 
 }
